06ac6215f06ea713085ca887e406aa280f6b9939
Mahnooranwar191 <mahnnooranwar191@gmail.com>
2025-07-07 21:00:37 +0500

    a

---
+++ asset/js/script.js
@@ -0,0 +1,38 @@
+var nav_btn = document.getElementById("nav");
+var nav = document.getElementsByClassName("navbar")[0];
+var tl = gsap.timeline();
+nav_btn.addEventListener("click", function () {
+  nav.style.display = nav.style.display === "block" ? "none" : "block";
+  nav_btn.classList.toggle("ri-close-line");
+  nav_btn.classList.toggle("ri-menu-3-line");
+});
+
+tl.from("#header h2", {
+  duration: 2,
+  y: -100,
+  stragger: 1,
+  opacity: 0.5,
+});
+tl.from("#header .navbar ul li a", {
+ duration: 2,
+  y: -100,
+  stragger: 1,
+  opacity: 0,
+});
+tl.from("#header i", {
+  duration: 1,
+  y: -100,
+ opacity: 0.5,
+});
+tl.from("#bannar .right-wrapper", {
+  duration: 1,
+  x: -200,
+  opacity: 0,
+  ease: "power2.out"
+})
+tl.from("#bannar .left-wrapper img", {
+  duration: 1,
+  x: 200,
+  opacity: 0,
+  ease: "power2.out"
+})
