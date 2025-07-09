52cfd736dca162f37b5dbb7ba2e9fdf2a8cae4c0
Mahnooranwar191 <mahnnooranwar191@gmail.com>
2025-07-09 21:49:14 +0500

    9Julys

--- asset/js/script.js
+++ asset/js/script.js
@@ -37,6 +37,14 @@ tl.from("#bannar .left-wrapper img", {
   ease: "power2.out"
 })
 
+tl.from("#myBtn",{
+  duration: 1,
+  y: 10,
+  opacity: 1,
+ 
+  yoyo: true,
+  repeat: -1 // -1 means infinite repeat
+})
 
 
 
@@ -63,3 +71,22 @@ const swiper = new Swiper('.swiper', {
     clickable: true,
   },
 });
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
