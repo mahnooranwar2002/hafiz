9eb6b5377b1678f76b6bd4754c37d3bbcabfed57
Mahnooranwar191 <mahnnooranwar191@gmail.com>
2025-07-12 13:37:22 +0500

    aj ka kam

---
+++ asset/js/projects.js
@@ -0,0 +1,282 @@
+var project = [
+  {
+    pName:
+      "Google Ads and Social Media Marketing for London-Based Dental Clinic",
+    CName: "Bright Smile Dental Care – London, UK",
+    PObjectives:
+      "Generate high-quality leads for dental services (braces, whitening, implants) Drive traffic to clinic website and appointment booking page Increase local brand visibility in the Greater London area",
+    TargetAudience: " 25–55",
+    image:
+      "https://t4.ftcdn.net/jpg/02/94/86/43/360_F_294864304_JVjEWTYCJFg7wJuLna9ISWiulsjuyzm7.jpg",
+    Gender: "All",
+    Location: " Central and Greater London",
+    Interests: "Healthcare, Personal Wellness, Cosmetic Dentistry",
+    Language: "English",
+
+    PlatformsUsed: [
+      "Google Ads (Search & Display)",
+      "Facebook & Instagram (Meta Ads)",
+      "Google My Business Optimization Website landing page tracking (Google Analytics & Tag Manager)",
+    ],
+    Services:
+      "Keyword Research & Competitor Analysis (UK-based) Google Ads Campaign Creation (with Ad Extensions) Facebook + Instagram Ads (Carousel, Video, Local Awareness) Location Targeting & Device Optimization Daily Budget Allocation & Bidding Strategy Setup Custom Conversion Tracking via Google Tag ManagerWeekly Analytics Reporting via Google Looker Studio",
+
+    ToolsUsed: [
+      "Google Ads",
+      "Meta Ads Manager",
+      "Google Analytics 4",
+      "Canva Pro for Creatives",
+      "Tag Manager",
+      "Google Looker Studio (for reports)",
+    ],
+    Result: [
+      " Over 3,500 website visits in 30 days",
+      " 170+ lead form submissions for appointments",
+      " CTR: 6.2% (above industry average)",
+      " Lowered CPC from £1.80 to £1.05 through optimization",
+      " Client booked 40+ actual appointments (confirmed)",
+    ],
+  },
+  {
+    pName: "Instagram  Facebook Ads for UK-Based Online Clothing Store",
+    CName: "Urban Vogue – Online Boutique (Manchester, UK)",
+    image:
+      "https://t4.ftcdn.net/jpg/02/94/86/43/360_F_294864304_JVjEWTYCJFg7wJuLna9ISWiulsjuyzm7.jpg",
+    PObjectives:
+      "Drive sales of summer clothing collection Retarget cart abandoners and existing customers Build brand following on Instagram",
+    Age: "18–35",
+    Gender: "Female",
+
+    Location: " UK-wide",
+    Interests: "Fashion, Shopping, Instagram influencers",
+    Language: "English",
+
+    PlatformsUsed: [
+      "Instagram Ads",
+
+      "Facebook Ads",
+
+      "Meta Pixel on Shopify",
+
+      "WhatsApp for order support",
+    ],
+    Services:
+      "Created ad creatives (carousel & reels) Product-based targeting & custom audience setup Retargeting strategy using Meta Pixel Influencer-style caption writing Ad performance tracking + A/B testing",
+
+    ToolsUsed: [
+      "Meta Ads Manager",
+      "Shopify Analytics",
+      "Canva for Reels & Stories",
+      "Google Sheets for reporting",
+      ,
+    ],
+    Result: [
+      " 48,000+ impressions",
+
+      "1,900+ link clicks",
+
+      "135 purchases (confirmed via Shopify backend)",
+
+      "ROAS: 4.2X",
+    ],
+  },
+  {
+    pName: "Google Ads for Real Estate Agent in Texas, USA",
+    CName: "Google Ads for Real Estate Agent in Texas, USA",
+    image:
+      "https://t4.ftcdn.net/jpg/02/94/86/43/360_F_294864304_JVjEWTYCJFg7wJuLna9ISWiulsjuyzm7.jpg",
+    PObjectives:
+      "Generate buyer & seller leads for residential properties Drive traffic to real estate landing pages Build local brand presence in Austin area",
+    Age: "30–60",
+    Gender: "Female",
+
+    Location: "Austin, Round Rock, Cedar Park ",
+    Interests: "Austin, Round Rock, Cedar Park",
+    Language: "English",
+
+    PlatformsUsed: [
+      "Google Search Ads",
+
+      "Google Display Network",
+
+      "Google My Business Profile",
+
+      "Custom landing page",
+    ],
+    Services:
+      "Keyword research & competitor targeting Created search & display ad campaigns Conversion tracking using Google Tag Manager Weekly lead reports in Google Sheets Designed 2 custom landing pages for buyer/seller",
+
+    ToolsUsed: [
+      "Google Ads",
+
+      "Tag Manager",
+
+      "Canva (for display ads)",
+
+      "Google Analytics 4",
+    ],
+    Result: [
+      "✅ 3,100+ targeted clicks",
+
+      "✅ 220+ form leads generated",
+
+      "✅ Cost per lead: $3.63 (vs. market avg. $7+)",
+
+      "✅ ROAS: 3.9X",
+    ],
+  },
+  {
+    pName: "Google Ads for Real Estate Agent in Texas, USA",
+    CName: "Google Ads for Real Estate Agent in Texas, USA",
+    image:
+      "https://t4.ftcdn.net/jpg/02/94/86/43/360_F_294864304_JVjEWTYCJFg7wJuLna9ISWiulsjuyzm7.jpg",
+    PObjectives:
+      "Generate buyer & seller leads for residential properties Drive traffic to real estate landing pages Build local brand presence in Austin area",
+    Age: "30–60",
+    Gender: "Female",
+
+    Location: "Austin, Round Rock, Cedar Park ",
+    Interests: "Austin, Round Rock, Cedar Park",
+    Language: "English",
+
+    PlatformsUsed: [
+      "Google Search Ads",
+
+      "Google Display Network",
+
+      "Google My Business Profile",
+
+      "Custom landing page",
+    ],
+    Services:
+      "Keyword research & competitor targeting Created search & display ad campaigns Conversion tracking using Google Tag Manager Weekly lead reports in Google Sheets Designed 2 custom landing pages for buyer/seller",
+
+    ToolsUsed: [
+      "Google Ads",
+
+      "Tag Manager",
+
+      "Canva (for display ads)",
+
+      "Google Analytics 4",
+    ],
+    Result: [
+      "✅ 3,100+ targeted clicks",
+
+      "✅ 220+ form leads generated",
+
+      "✅ Cost per lead: $3.63 (vs. market avg. $7+)",
+
+      "✅ ROAS: 3.9X",
+    ],
+  },
+  {
+    pName: "Facebook Ads for Beauty Salon in Los Angeles, USA",
+    CName: "Glow & Go Beauty Lounge – Los Angeles, California",
+    image:
+      "https://t4.ftcdn.net/jpg/02/94/86/43/360_F_294864304_JVjEWTYCJFg7wJuLna9ISWiulsjuyzm7.jpg",
+    PObjectives:
+      "Attract new clients for facials & skincare packages Promote limited-time discounts Increase Instagram followers and reviews",
+    Age: "20-45",
+    Gender: "Female",
+
+    Location: "10-mile radius in L.A. ",
+    Interests: "Skincare, facials, beauty influencers",
+    Language: "English",
+
+    PlatformsUsed: [
+      "Facebook & Instagram Ads",
+
+      "Google My Business",
+
+      "Yelp Optimization",
+    ],
+    Services:
+      "Created carousel ads with skincare packages Location-based targeting within LA Custom captions with strong call-to-action Integrated Messenger for instant bookings Managed review replies on Yelp & Google",
+
+    ToolsUsed: [
+      "Facebook & Instagram Ads",
+
+      "Google My Business",
+
+      "Yelp Optimization",
+    ],
+    Result: [
+      "✅ Reached 42,000+ local women in 3 weeks",
+
+      "✅ 350+ new Instagram followers",
+
+      "✅ 120 new booking inquiries",
+
+      "✅ Avg. CPC: $0.41",
+    ],
+  },
+  {
+    pName: "Complete SEO Optimization for E-Commerce Jewelry Brand – USA",
+    CName: "LuxeGems – Online Jewelry Store (New York, USA)",
+    image:
+      "https://t4.ftcdn.net/jpg/02/94/86/43/360_F_294864304_JVjEWTYCJFg7wJuLna9ISWiulsjuyzm7.jpg",
+    PObjectives:
+      "Improve Google search rankings for product keywords Increase organic traffic to product & collection pages Boost domain authority and reduce dependency on paid ads",
+    Age: "25–55",
+    Gender: "Female",
+
+    Location: "United States",
+    Interests: "Jewelry, gifts, fashion accessories, weddings",
+    Language: "English",
+
+    PlatformsUsed: [
+      "Facebook & Instagram Ads",
+
+      "Google My Business",
+
+      "Yelp Optimization",
+    ],
+    Services:
+      "✅ On-Page SEO Keyword research (long-tail + buyer intent) Optimized meta titles, descriptions, H1/H2s Image ALT tags + compressing for speed URL structure cleanup and internal linking ✅ Technical SEO Fixed crawl errors (via Search Console) Improved Core Web Vitals XML sitemap + robots.txt updates Mobile responsiveness and speed optimization ✅ Off-Page SEO 25+ High-DA backlinks (blogs, profiles, directories) Guest posting on fashion blogs Social bookmarking and local citations ✅ Content Optimization Optimized 10 blog posts for SEOCreated 3 new articles targeting “best gifts under $100 FAQ schema integration for featured snippets",
+
+    ToolsUsed: [
+      "Google Search Console",
+
+      "Google Analytics 4",
+
+      "Ahrefs / SEMrush",
+
+      "Screaming Frog",
+
+      "Yoast SEO Plugin (for WordPress)",
+
+      "Canva (for blog banners)",
+    ],
+    Result: [
+      "✅ Organic traffic increased by +215%",
+
+      "✅ Ranked on Page 1 for 12+ keywords (e.g. “affordable diamond rings USA”)",
+
+      "✅ Bounce rate dropped from 72% → 48%",
+
+      "✅ Monthly sales from organic search grew by 2.4x",
+
+      "✅ Domain Rating increased from 16 → 27 (Ahrefs)",
+    ],
+  },
+];
+
+if (document.querySelector("#projectData")) {
+  for (var i = 0; i < project.length; i++) {
+    document.querySelector("#projectData").innerHTML += `
+   <div class="flip-card">
+  
+    
+      <img src="${project[i].image}" alt="Avatar" style="width:300px;height:300px;">
+    
+    <div class="flip-card-back">
+      <h4>${project[i].pName}</h4> 
+      <p>${project[i].PObjectives}
+</p> 
+    <div class="link"><a href="project-details.html?project=${project[i].pName}&image=${project[i].image}&CName=${project[i].CName}&PObjectives=${project[i].PObjectives}&TargetAudience=${project[i].TargetAudience}&Gender=${project[i].Gender}&Location=${project[i].Location}&Interests=${project[i].Interests}&Language=${project[i].Language}&PlatformsUsed=${project[i].PlatformsUsed}&Services=${project[i].Services}&ToolsUsed=${project[i].ToolsUsed}&Result=${project[i].Result}"><i class="ri-arrow-right-up-line"></i></a></div>
+    </div>
+
+</div>`;
+  }
+}
